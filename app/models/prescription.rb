8aa6e15e079dcc127315c0f5e76603a41b96ee5b
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-09-08 15:33:32 -0600

    fixed corrupt table info for all users.

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,4 +3,11 @@ class Prescription < ActiveRecord::Base
   belongs_to :medication
   belongs_to :patient
 
+  # validates :medication_id, presence: true
+  # validates :dosage, presence: true
+  # validates :schedule, presence: true
+  # validates :start_on, presence: true
+  # validates :end_on, presence: true
+
+
 end
